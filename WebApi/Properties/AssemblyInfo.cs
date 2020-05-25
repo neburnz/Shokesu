acbc4bb39b59fc81df724f7e9e31deb5d2802187
Rubén Guzmán <neburnz@hotmail.com>
2020-05-25 11:53:22 -0500

    Build Number Major Minor

--- WebApi/Properties/AssemblyInfo.cs
+++ WebApi/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("__Build.BuildNumber__")]
-[assembly: AssemblyFileVersion("__Build.BuildNumber__")]
+[assembly: AssemblyVersion("__Major__.__Minor__.__Build.BuildNumber__")]
+[assembly: AssemblyFileVersion("__Major__.__Minor__.__Build.BuildNumber__")]
