ee4acfaa1d1451179cbbe12afeae522fef85f7dd
Rubén Guzmán <neburnz@hotmail.com>
2020-05-25 11:41:59 -0500

    Build Number

--- WebApi/Properties/AssemblyInfo.cs
+++ WebApi/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("__Major__.__Minor__.0.0")]
-[assembly: AssemblyFileVersion("__Major__.__Minor__.0.0")]
+[assembly: AssemblyVersion("__Build.BuildNumber__")]
+[assembly: AssemblyFileVersion("__Build.BuildNumber__")]
